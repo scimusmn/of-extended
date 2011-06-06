ccf16c11630c12bd45ed8f3069574fa2bf8d9611
Aaron Heidgerken <aheidgerken@smm.org>
2011-06-06 10:55:27 -0500

    added ofExtended.h

---
+++ ofExtended.h
@@ -0,0 +1,34 @@
+/*
+ *  ofExtended.h
+ *  openFrameworksLib
+ *
+ *  Created by Exhibits on 1/31/2554.
+ *  Copyright 2011 Science Museum of Minnesota. All rights reserved.
+ *
+ */
+
+#include "ofMain.h"
+
+//--------------------------
+// interface
+#include "extended/interface/ofButton.h"
+#include "extended/interface/ofGrid.h"
+#include "extended/interface/ofDropDown.h"
+#include "extended/interface/ofTabs.h"
+#include "extended/interface/ofSpinner.h"
+#include "extended/interface/ofScrollBar.h"
+#include "extended/interface/ofRadioButtons.h"
+
+//--------------------------
+// vector math
+#include "extended/ofVector.h"
+#include "extended/interface/ofGraphicEx.h"
+
+//--------------------------
+// extended utility functions
+#include "extended/extendedUtils/ofTimer.h"
+#include "extended/extendedUtils/ofFont.h"
+#include "extended/fileFunctions/xml_smm.h"
+#include "extended/fileFunctions/ofXMLParse.h"
+#include "extended/parseFunctions/ofParsing.h"
+//#include "ofSystemCall.h"
